e0098c3a14723a7f494c7d1424b02ca12bc32019
hussien415532 <hussien415532@gmail.com>
2023-08-31 23:29:22 +0300

    edit

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
-const router = _router('db.json');
-const middlewares = defaults();
+import * as jsonServer from "json-server";
 
-const port = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use('http://localhost:3000/employees', router); // Set up a base route for the API
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+const app = jsonServer.create();
+app.use("/", jsonServer.router(require("./db.json")));
+app.listen(3000, () => console.log("JSON Server is running on port 3000"));
