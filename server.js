95b4a7df32d74e33c87b4c0665d5203c7024efa9
hussien415532 <hussien415532@gmail.com>
2023-09-01 11:25:44 +0300

    .

--- server.js
+++
@@ -1,5 +0,0 @@
-import * as jsonServer from "json-server";
-
-const app = jsonServer.create();
-app.use("/", jsonServer.router(require("./db.json")));
-app.listen(3000, () => console.log("JSON Server is running on port 3000"));
